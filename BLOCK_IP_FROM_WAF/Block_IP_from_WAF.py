140d3275ef8b5a1ffc161211533e9ccce0d16841
이재영 <101492831+jaeyeong16@users.noreply.github.com>
2024-01-05 23:10:03 +0900

    Update Block_IP_from_WAF.py

--- BLOCK_IP_FROM_WAF/Block_IP_from_WAF.py
+++ BLOCK_IP_FROM_WAF/Block_IP_from_WAF.py
@@ -238,3 +238,20 @@ def lambda_handler(event, context):
             'data': base64.b64encode(json.dumps(http_request_data).encode('utf-8')).decode('utf-8')
         })
     return {'records': output_records}
+function_name = 'Discord-bot_send'
+region_name = 'ap-northeast-2'  # 서울 리전 코드
+input_data = 'IMDS-IAM'
+
+# AWS Lambda 클라이언트를 생성합니다.
+lambda_client = boto3.client('lambda', region_name=region_name)
+
+# Lambda 함수를 호출합니다.
+response = lambda_client.invoke(
+    FunctionName=function_name,
+    InvocationType='RequestResponse',  # 또는 'Event'로 설정하여 비동기적으로 호출할 수 있습니다.
+    Payload=json.dumps({"content" : "Block_IP_from_WAF 함수 동작 "}),
+    LogType='Tail'  # Lambda 로그를 확인하려면 'Tail'을 선택합니다.
+)
+
+# 호출 결과를 출력합니다.
+print(response['Payload'].read().decode())
