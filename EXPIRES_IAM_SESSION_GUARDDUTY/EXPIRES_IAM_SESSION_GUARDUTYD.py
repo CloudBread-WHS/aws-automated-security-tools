60a9386f94c78ed6e7db6beaf23479477c985fbb
janie71 <125464850+janie71@users.noreply.github.com>
2024-01-05 15:25:55 +0900

    Update EXPIRES_IAM_SESSION_GUARDUTYD.py

--- EXPIRES_IAM_SESSION_GUARDDUTY/EXPIRES_IAM_SESSION_GUARDUTYD.py
+++ EXPIRES_IAM_SESSION_GUARDDUTY/EXPIRES_IAM_SESSION_GUARDUTYD.py
@@ -5,7 +5,6 @@ def lambda_handler(event, context):
     # IAM 사용자 이름
     iam_user = event['detail']['Resource']['AccessKeyDetails']['UserName']
     
-    
     # IAM 사용자의 세션 강제 만료
     iam_client = boto3.client('iam')
 
@@ -14,7 +13,6 @@ def lambda_handler(event, context):
             UserName=iam_user,
             PolicyName='imds_policy',
             PolicyDocument= '{"Version":"2012-10-17","Statement":[{"Effect":"Deny","Action":"*","Resource": "*"}]}'
-
         )
         
         return {
